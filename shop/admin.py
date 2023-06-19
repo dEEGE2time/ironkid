efdf3768f9d71567821e200e378e6a937516a267
dEEGE2time <>
2023-06-19 17:34:38 +0000

    add ProductAdmin and CommentAdmin models

--- shop/admin.py
+++ shop/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Product)
 class ProductAdmin(SummernoteModelAdmin):
+    list_filter = ('category', 'status') # add 'created_on' later
+    list_display = ('name', 'slug', 'status') # add 'created_on' later
+    search_fields = ['name', 'description']
     summernote_fields = ('description')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_filter = ('approved', 'created_on')
+    list_display = ('username', 'body', 'post', 'created_on', 'approved')
+    search_fields = ['username', 'email', 'body']
+    actions = ['approved_comments',]
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
