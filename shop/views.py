a1b5e5d45229a9c8e868e3a8efafaffb6ec609be
dEEGE2time <>
2023-06-20 18:24:15 +0000

    create view, add template html and connect urls

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Product
 
-# Create your views here.
+class ProductList(generic.ListView):
+    model = Product
+    paginate_by = 10
+    queryset = Product.objects.filter(status=1).order_by('category')
+    template_name = 'shop.html'
