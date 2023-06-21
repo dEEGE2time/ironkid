e20fd2339c6877c595d106b8d067f90b48b43e8f
dEEGE2time <>
2023-06-21 20:12:58 +0000

    install allauth and connect urls

--- shop/views.py
+++ shop/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Product
 
+
 class ProductList(generic.ListView):
     model = Product
     paginate_by = 10
     queryset = Product.objects.filter(status=1).order_by('category')
     template_name = 'shop.html'
+
 
 class ProductDetails(View):
 
