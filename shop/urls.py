6c494bf3df727b665c7db5d72baa7265c724e925
dEEGE2time <>
2023-06-21 18:20:22 +0000

    create ProductDetails view, connect urls, minor styling

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.ProductList.as_view(), name='shop')
+    path('', views.ProductList.as_view(), name='shop'),
+    path('<slug:slug>/', views.ProductDetails.as_view(), name='product_detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Product
 
 class ProductList(generic.ListView):
@@ -7,3 +7,23 @@ class ProductList(generic.ListView):
     paginate_by = 10
     queryset = Product.objects.filter(status=1).order_by('category')
     template_name = 'shop.html'
+
+class ProductDetails(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Product.objects.filter(status=1)
+        product = get_object_or_404(queryset, slug=slug)
+        comments = product.comments.filter(approved=True).order_by('created_on')
+        bookmarked = False
+        if product.bookmarks.filter(id=self.request.user.id).exists():
+            bookmarked = True
+
+        return render(
+            request,
+            'product_detail.html',
+            {
+                'product': product,
+                'comments': comments,
+                'bookmarked': bookmarked   
+            },
+        )
